32ccd388cba2368d5ab5f548174acdb42a7a8eca
Zijian Zhang <futrime+git@outlook.com>
2025-01-05 10:04:46 +0800

    feat: migrate to .NET 9.0

---
+++ Lip/PackageManifest.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Lip;
+
+public record PackageManifest
+{
+    [JsonPropertyName("format_version")]
+    public required string FormatVersion;
+
+    [JsonPropertyName("format_uuid")]
+    public required string FormatUuid;
+
+    [JsonPropertyName("tooth")]
+    public required string Tooth;
+
+    [JsonPropertyName("version")]
+    public required string Version;
+
+    [JsonPropertyName("info")]
+    public InfoType? Info;
+
+    public record InfoType
+    {
+        [JsonPropertyName("name")]
+        public string? Name;
+
+        [JsonPropertyName("description")]
+        public string? Description;
+    }
+}

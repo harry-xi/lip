c96f84da5a97424406ad8811dce6e21a5b534110
futrime <futrime+github.com@outlook.com>
2025-01-09 17:36:30 +0800

    refactor: encoding seet to UTF-8 and line break set to LF

--- Lip.Tests/Lip.InitTests.cs
+++ Lip.Tests/Lip.InitTests.cs
@@ -1,10 +1,12 @@
-﻿using System.IO.Abstractions.TestingHelpers;
+using System.IO.Abstractions.TestingHelpers;
 using Moq;
 
 namespace Lip.Tests;
 
 public partial class LipTests
 {
+    private static readonly string s_workspacePath = OperatingSystem.IsWindows() ? Path.Combine("C:", "path", "to", "workspace") : Path.Combine("/", "path", "to", "workspace");
+
     public static Mock<Serilog.ILogger> CreateMockLogger()
     {
         var logger = new Mock<Serilog.ILogger>();
@@ -18,8 +20,8 @@ public partial class LipTests
     {
         MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
         {
-            { @"C:\path\to\workspace", new MockDirectoryData() },
-        }, currentDirectory: @"C:\path\to\workspace");
+            { s_workspacePath, new MockDirectoryData() },
+        }, currentDirectory: s_workspacePath);
         Mock<Serilog.ILogger> logger = CreateMockLogger();
         Lip lip = new(new(), fileSystem, logger.Object);
         Lip.InitArgs args = new()
@@ -36,7 +38,7 @@ public partial class LipTests
         await lip.Init(args);
 
         logger.Verify(l => l.Information(It.IsAny<string>()), Times.Exactly(1));
-        Assert.True(fileSystem.File.Exists(@"C:\path\to\workspace\tooth.json"));
+        Assert.True(fileSystem.File.Exists(Path.Combine(s_workspacePath, "tooth.json")));
         Assert.Equal("""
             {
                 "format_version": 3,
@@ -50,7 +52,7 @@ public partial class LipTests
                     "avatar_url": "https://example.com/avatar.png"
                 }
             }
-            """, fileSystem.File.ReadAllText(@"C:\path\to\workspace\tooth.json"));
+            """, fileSystem.File.ReadAllText(Path.Combine(s_workspacePath, "tooth.json")));
     }
 
     [Fact]
@@ -58,8 +60,8 @@ public partial class LipTests
     {
         MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
         {
-            { @"C:\path\to\workspace", new MockDirectoryData() },
-        }, currentDirectory: @"C:\path\to\workspace");
+            { s_workspacePath, new MockDirectoryData() },
+        }, currentDirectory: s_workspacePath);
         Mock<Serilog.ILogger> logger = CreateMockLogger();
         Lip lip = new(new(), fileSystem, logger.Object);
         Lip.InitArgs args = new();
@@ -98,7 +100,7 @@ public partial class LipTests
         await lip.Init(args);
 
         logger.Verify(l => l.Information(It.IsAny<string>()), Times.Exactly(1));
-        Assert.True(fileSystem.File.Exists(@"C:\path\to\workspace\tooth.json"));
+        Assert.True(fileSystem.File.Exists(Path.Combine(s_workspacePath, "tooth.json")));
         Assert.Equal("""
             {
                 "format_version": 3,
@@ -112,7 +114,7 @@ public partial class LipTests
                     "avatar_url": "https://example.com/avatar.png"
                 }
             }
-            """, fileSystem.File.ReadAllText(@"C:\path\to\workspace\tooth.json"));
+            """, fileSystem.File.ReadAllText(Path.Combine(s_workspacePath, "tooth.json")));
     }
 
     [Fact]
@@ -120,8 +122,8 @@ public partial class LipTests
     {
         MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
         {
-            { @"C:\path\to\workspace", new MockDirectoryData() },
-        }, currentDirectory: @"C:\path\to\workspace");
+            { s_workspacePath, new MockDirectoryData() },
+        }, currentDirectory: s_workspacePath);
         Mock<Serilog.ILogger> logger = CreateMockLogger();
         Lip lip = new(new(), fileSystem, logger.Object);
         Lip.InitArgs args = new();
@@ -161,49 +163,9 @@ public partial class LipTests
     }
 
     [Fact]
-    public async Task Init_POSIX_Passes()
-    {
-        MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
-        {
-            { "/path/to/workspace", new MockDirectoryData() },
-        }, currentDirectory: "/path/to/workspace");
-        Mock<Serilog.ILogger> logger = CreateMockLogger();
-        Lip lip = new(new(), fileSystem, logger.Object);
-        Lip.InitArgs args = new()
-        {
-            InitAuthor = "Example Author",
-            InitAvatarUrl = "https://example.com/avatar.png",
-            InitDescription = "An example package.",
-            InitName = "Example Package",
-            InitTooth = "example.com/org/package",
-            InitVersion = "0.1.0",
-            Yes = true,
-        };
-
-        await lip.Init(args);
-
-        logger.Verify(l => l.Information(It.IsAny<string>()), Times.Exactly(1));
-        Assert.True(fileSystem.File.Exists("/path/to/workspace/tooth.json"));
-        Assert.Equal("""
-            {
-                "format_version": 3,
-                "format_uuid": "289f771f-2c9a-4d73-9f3f-8492495a924d",
-                "tooth": "example.com/org/package",
-                "version": "0.1.0",
-                "info": {
-                    "name": "Example Package",
-                    "description": "An example package.",
-                    "author": "Example Author",
-                    "avatar_url": "https://example.com/avatar.png"
-                }
-            }
-            """, fileSystem.File.ReadAllText("/path/to/workspace/tooth.json"));
-    }
-
-    [Fact]
     public async Task Init_WorkspaceDoesNotExist_ThrowsDirectoryNotFoundException()
     {
-        MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>(), currentDirectory: @"C:\path\to\workspace");
+        MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>(), currentDirectory: s_workspacePath);
         Mock<Serilog.ILogger> logger = CreateMockLogger();
         Lip lip = new(new(), fileSystem, logger.Object);
         Lip.InitArgs args = new()
@@ -220,9 +182,9 @@ public partial class LipTests
     {
         MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
         {
-            { @"C:\path\to\workspace", new MockDirectoryData() },
-            { @"C:\path\to\workspace\tooth.json", new MockFileData("content") },
-        }, currentDirectory: @"C:\path\to\workspace");
+            { s_workspacePath, new MockDirectoryData() },
+            { Path.Combine(s_workspacePath, "tooth.json"), new MockFileData("content") },
+        }, currentDirectory: s_workspacePath);
         Mock<Serilog.ILogger> logger = CreateMockLogger();
         Lip lip = new(new(), fileSystem, logger.Object);
         Lip.InitArgs args = new()
@@ -239,9 +201,9 @@ public partial class LipTests
     {
         MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
         {
-            { @"C:\path\to\workspace", new MockDirectoryData() },
-            { @"C:\path\to\workspace\tooth.json", new MockFileData("content") },
-        }, currentDirectory: @"C:\path\to\workspace");
+            { s_workspacePath, new MockDirectoryData() },
+            { Path.Combine(s_workspacePath, "tooth.json"), new MockFileData("content") },
+        }, currentDirectory: s_workspacePath);
         Mock<Serilog.ILogger> logger = CreateMockLogger();
         Lip lip = new(new(), fileSystem, logger.Object);
         Lip.InitArgs args = new()
@@ -255,6 +217,6 @@ public partial class LipTests
 
         logger.Verify(l => l.Information(It.IsAny<string>()), Times.Exactly(1));
         logger.Verify(l => l.Warning(It.IsAny<string>()), Times.Exactly(1));
-        Assert.True(fileSystem.File.Exists(@"C:\path\to\workspace\tooth.json"));
+        Assert.True(fileSystem.File.Exists(Path.Combine(s_workspacePath, "tooth.json")));
     }
 }

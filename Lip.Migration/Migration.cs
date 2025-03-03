c1c1969f559250c37bfc2ebba8043faecce2cef7
Zijian <35801754+futrime@users.noreply.github.com>
2025-03-03 17:09:20 +0800

    feat: add Lip.Migration

---
+++ Lip.Migration/Migration.cs
@@ -0,0 +1,11 @@
+using System.Text.Json;
+
+namespace Lip.Migration;
+
+public static class Migration
+{
+    public static JsonElement Migrate(JsonElement json)
+    {
+        return json;
+    }
+}

a5c277d039b961c36229be21606881e4177ea3e0
Zijian <35801754+futrime@users.noreply.github.com>
2025-03-08 11:27:54 +0800

    chore: show curl output during download for install_macos.sh

--- scripts/install_macos.sh
+++ scripts/install_macos.sh
@@ -21,7 +21,7 @@ DOWNLOAD_URL="$GITHUB_MIRROR/futrime/lip/releases/latest/download/lip-osx-$PLATF
 # Download the release.
 TEMP_DIR=$(mktemp -d)
 echo "Downloading $DOWNLOAD_URL"
-curl -fLsS $DOWNLOAD_URL | tar -x -C $TEMP_DIR
+curl -fLS $DOWNLOAD_URL | tar -x -C $TEMP_DIR
 
 # Install the binary.
 INSTALL_DIR="$HOME/Library/Application\ Support/lip"

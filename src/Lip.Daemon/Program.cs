50f85ba69e6e774b717d5bceb2a7aa5cd48db3f3
Zijian Zhang <35801754+futrime@users.noreply.github.com>
2026-02-12 23:00:22 +0800

    feat: add lip daemon

---
+++ src/Lip.Daemon/Program.cs
@@ -0,0 +1,19 @@
+﻿using Lip.Core.PublicApi;
+using Lip.Daemon;
+using StreamJsonRpc;
+
+using JsonRpc rpc = new(
+    Console.OpenStandardInput(),
+    Console.OpenStandardOutput());
+
+IClientContract clientProxy = rpc.Attach<IClientContract>();
+
+RpcUserInteraction userInteraction = new(clientProxy);
+
+LipClient client = await LipClient.Create(userInteraction);
+
+rpc.AddLocalRpcTarget(client);
+
+rpc.StartListening();
+
+await rpc.Completion;
---
+++ src/Lip.Daemon/RpcUserInteraction.cs
@@ -0,0 +1,40 @@
+using Lip.Core.Infrastructure;
+
+namespace Lip.Daemon;
+
+public class RpcUserInteraction(IClientContract clientContract) : IUserInteraction
+{
+    private readonly IClientContract _clientContract = clientContract;
+
+    public async Task PrintError(string message)
+    {
+        await _clientContract.PrintError(message);
+    }
+
+    public async Task PrintInfo(string message)
+    {
+        await _clientContract.PrintInfo(message);
+    }
+
+    public async Task PrintSuccess(string message)
+    {
+        await _clientContract.PrintSuccess(message);
+    }
+
+    public async Task PrintWarning(string message)
+    {
+        await _clientContract.PrintWarning(message);
+    }
+
+    public async Task RunWithProgress(string message, Func<IProgress<double>, Task> action)
+    {
+        string progressId = Guid.NewGuid().ToString();
+
+        Progress<double> progress = new(p =>
+        {
+            _clientContract.ReportProgress(progressId, message, p);
+        });
+
+        await action(progress);
+    }
+}

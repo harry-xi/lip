26431bb4102d4257e1a9eda8c86985c8ad248f01
Zijian Zhang <35801754+futrime@users.noreply.github.com>
2026-03-03 12:17:07 +0800

    test: add regression tests for go module proxy

---
+++ tests/Lip.Core.RegressionTests/PackageRegistries/GoModuleProxyPackageRegistryTests.cs
@@ -0,0 +1,25 @@
+using Flurl;
+using Lip.Core.Entities;
+using Lip.Core.Infrastructure;
+using Lip.Core.PackageRegistries;
+using Semver;
+
+namespace Lip.Core.RegressionTests.PackageRegistries;
+
+public class GoModuleProxyPackageRegistryTests
+{
+    [Theory]
+    [InlineData("github.com/LiteLDev/bds", 158)]
+    [InlineData("github.com/LiteLDev/LegacyScriptEngine", 110)]
+    [InlineData("github.com/LiteLDev/LeviLamina", 85)]
+    [InlineData("github.com/LiteLDev/LeviLamina#client", 10)]
+    public async Task GetAvailableVersions_ReturnsSortedMinCount(string packageId, int minVersionCount)
+    {
+        GoModuleProxyPackageRegistry registry = new(new Url("https://goproxy.io"));
+
+        IOrderedEnumerable<SemVersion> versions = await registry.GetAvailableVersions(PackageId.Parse(packageId));
+
+        Assert.True(versions.Count() >= minVersionCount);
+        Assert.Equal(versions, versions.Order(SemVersion.PrecedenceComparer));
+    }
+}

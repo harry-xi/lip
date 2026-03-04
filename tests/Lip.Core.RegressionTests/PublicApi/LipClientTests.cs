73bb58c001a9d840b4d506f3f25965610a3d1b25
Zijian Zhang <35801754+futrime@users.noreply.github.com>
2026-03-04 22:24:05 +0800

    test: add more test cases

--- tests/Lip.Core.RegressionTests/PublicApi/LipClientTests.cs
+++ tests/Lip.Core.RegressionTests/PublicApi/LipClientTests.cs
@@ -10,6 +10,28 @@ public class LipClientTests {
   public record WorkflowUninstallStep(List<string> Packages) : WorkflowBaseStep;
   public record WorkflowUpdateStep(List<string> Packages) : WorkflowBaseStep;
 
+  private class TempWorkdir : IDisposable {
+    private readonly string _originalDir = Directory.GetCurrentDirectory();
+    private readonly string _tempDir = Path.Join(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+    public TempWorkdir() {
+      Directory.CreateDirectory(_tempDir);
+
+      Directory.SetCurrentDirectory(_tempDir);
+    }
+
+    public void Dispose() {
+      Directory.SetCurrentDirectory(_originalDir);
+
+      try {
+        Directory.Delete(_tempDir, recursive: true);
+      }
+      catch {
+        // Ignore any exceptions during cleanup
+      }
+    }
+  }
+
   public static TheoryData<List<string>> Install_OnWinX64_DoesNotThrowData =>
   [
     ["github.com/LiteLDev/bds@1.26.3"],
@@ -24,39 +46,64 @@ public class LipClientTests {
   public async Task Install_OnWinX64_DoesNotThrow(List<string> packages) {
     IUserInteraction userInteraction = CreateMockUserInteraction();
 
-    string workspaceDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-    Directory.CreateDirectory(workspaceDir);
-
-    Directory.SetCurrentDirectory(workspaceDir);
+    using TempWorkdir _ = new();
 
     LipClient lipClient = await LipClient.Create(userInteraction);
 
     await lipClient.Install(packages, dryRun: false, ignoreScripts: false, noDependencies: false);
   }
 
-  public static TheoryData<List<WorkflowBaseStep>> ComplexWorkflow_OnWinX64_DoesNotThrowData =>
+  public static TheoryData<(List<WorkflowBaseStep> Steps, List<string> FinalExplicitPackages)> ComplexWorkflow_OnWinX64_DoesNotThrowData =>
   [
-    [
-      new WorkflowInstallStep(["github.com/LiteLDev/LeviLamina@1.9.7", "github.com/LiteLDev/LegacyScriptEngine@0.17.5"]),
-      new WorkflowUninstallStep(["github.com/LiteLDev/LegacyScriptEngine"]),
-      new WorkflowInstallStep(["github.com/LiteLDev/MoreDimensions@0.13.0"]),
-      new WorkflowInstallStep(["github.com/LiteLDev/LegacyScriptEngine@0.17.5"]),
-    ]
+    (
+      [
+        new WorkflowInstallStep(["github.com/LiteLDev/LeviLamina@1.9.7", "github.com/LiteLDev/LegacyScriptEngine@0.17.5"]),
+        new WorkflowUninstallStep(["github.com/LiteLDev/LegacyScriptEngine"]),
+        new WorkflowInstallStep(["github.com/LiteLDev/MoreDimensions@0.13.0"]),
+        new WorkflowInstallStep(["github.com/LiteLDev/LegacyScriptEngine@0.17.5"]),
+      ],
+      [
+        "github.com/LiteLDev/LeviLamina@1.9.7",
+        "github.com/LiteLDev/MoreDimensions@0.13.0",
+        "github.com/LiteLDev/LegacyScriptEngine@0.17.5"
+      ]
+    ),
+    (
+      [
+        new WorkflowInstallStep(["github.com/LiteLDev/LeviLamina@1.9.7"]),
+        new WorkflowInstallStep(["github.com/LiteLDev/LegacyMoney@0.17.0"]),
+        new WorkflowInstallStep(["github.com/LiteLDev/LegacyScriptEngine@0.17.5"]),
+        new WorkflowUninstallStep(["github.com/LiteLDev/LegacyMoney"]),
+      ],
+      [
+        "github.com/LiteLDev/LeviLamina@1.9.7",
+        "github.com/LiteLDev/LegacyScriptEngine@0.17.5"
+      ]
+    ),
+    (
+      [
+        new WorkflowInstallStep(["github.com/LiteLDev/LeviLamina@1.9.7"]),
+        new WorkflowInstallStep(["github.com/LiteLDev/LegacyScriptEngine@0.17.5"]),
+        new WorkflowInstallStep(["github.com/LiteLDev/LegacyMoney@0.17.0"]),
+        new WorkflowUninstallStep(["github.com/LiteLDev/LegacyMoney"]),
+      ],
+      [
+        "github.com/LiteLDev/LeviLamina@1.9.7",
+        "github.com/LiteLDev/LegacyScriptEngine@0.17.5"
+      ]
+    )
   ];
 
   [WinX64Theory]
   [MemberData(nameof(ComplexWorkflow_OnWinX64_DoesNotThrowData))]
-  public async Task ComplexWorkflow_OnWinX64_DoesNotThrow(List<WorkflowBaseStep> steps) {
+  public async Task ComplexWorkflow_OnWinX64_DoesNotThrow((List<WorkflowBaseStep> Steps, List<string> FinalExplicitPackages) testCase) {
     IUserInteraction userInteraction = CreateMockUserInteraction();
 
-    string workspaceDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-    Directory.CreateDirectory(workspaceDir);
-
-    Directory.SetCurrentDirectory(workspaceDir);
+    using TempWorkdir _ = new();
 
     LipClient lipClient = await LipClient.Create(userInteraction);
 
-    foreach (var step in steps) {
+    foreach (var step in testCase.Steps) {
       switch (step) {
         case WorkflowInstallStep installStep:
           await lipClient.Install(installStep.Packages, dryRun: false, ignoreScripts: false, noDependencies: false);
@@ -74,6 +121,11 @@ public class LipClientTests {
           throw new NotSupportedException("Unknown workflow step type");
       }
     }
+
+    (IEnumerable<string>? explicitPackages, var _) = await lipClient.List();
+
+    Assert.NotNull(explicitPackages);
+    Assert.Equal(testCase.FinalExplicitPackages.Order(), explicitPackages.Order());
   }
 
   private static IUserInteraction CreateMockUserInteraction() {
